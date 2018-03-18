7f25f9ae83864426eabd85009867714e944e1540
Artem <zltkhnrtm@tutanota.de>
2018-03-18 21:46:49 +0300

    openmp_tasks_1:11

---
+++ openmp_tasks_1/11.cpp
@@ -0,0 +1,41 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits>
+
+
+#define RANDOM_TOP 70
+#define THREADS 4
+#define DIVIDER 7
+#define SIZE 20
+
+using namespace std;
+
+int main() {
+
+    omp_set_dynamic(0);
+    omp_set_num_threads(THREADS);
+
+    int a[SIZE];
+
+    for (int i = 0; i < SIZE; i++){
+        a[i] = rand() % RANDOM_TOP;
+        printf("%d ", a[i]);
+    }
+
+    int max = numeric_limits<int>::min();
+    
+    #pragma omp parallel for shared(max)
+    for (int i = 0; i < SIZE; i++) {
+        if (a[i] % DIVIDER == 0) {
+            if (a[i] > max) {
+                #pragma omp critical
+                {
+                    max = a[i];
+                }
+            }
+        }
+    }
+
+    printf("Max %%7 : %d\n", max);
+}
