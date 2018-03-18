62e585692b8878555eda0fe7ed0ed091cb427d4d
Artem <zltkhnrtm@tutanota.de>
2018-03-18 22:14:22 +0300

    openmp_tasks_1:14

---
+++ openmp_tasks_1/14.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <omp.h>
+#include <stdio.h>
+
+#define NUM_THREADS 4
+#define N 210
+
+using namespace std;
+
+int main() {
+    
+    omp_set_dynamic(0); // not change number of threads when running
+    omp_set_num_threads(NUM_THREADS);
+    
+    int result = 0;
+
+    #pragma omp parallel for reduction(+:result)
+    for (int i = 0; i < N; i ++) {
+        result += 2 * i + 1;
+    }
+
+    printf("%d\n", result);
+
+    return 0;
+}
